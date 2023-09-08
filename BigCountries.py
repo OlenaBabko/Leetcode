615ec8b116fb51736213889d5629c5929c17d2ef
ljolia <elena.babko@gmail.com>
2023-09-08 09:21:26 +0300

    find big countries with SQL

--- BigCountries.py
+++ BigCountries.py
@@ -13,3 +13,11 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
   big_countries_df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
   return big_countries_df[['name', 'population', 'area']]
+
+
+
+# Write your MySQL query statement below
+
+# SELECT name, population, area
+# FROM World
+# WHERE (population >= 25000000) OR (area >= 3000000) ;
