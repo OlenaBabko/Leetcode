b4d07044a7fba7c330c6b9f61bb0d90de21dbd82
ljolia <elena.babko@gmail.com>
2023-09-08 09:20:29 +0300

    find big countries with pandas

---
+++ BigCountries.py
@@ -0,0 +1,15 @@
+# name is the primary key (column with unique values) for this table.
+# Each row of this table gives information about the name of a country,
+# the continent to which it belongs, its area, the population, and its GDP value.
+# A country is big if:
+# it has an area of at least three million (i.e., 3000000 km2), or
+# it has a population of at least twenty-five million (i.e., 25000000).
+# Write a solution to find the name, population, and area of the big countries.
+
+import pandas as pd
+
+# name >=3000000 km2 OR >=25000000
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+  big_countries_df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+  return big_countries_df[['name', 'population', 'area']]
