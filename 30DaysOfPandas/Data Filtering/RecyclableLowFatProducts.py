3080ace362da8b6135d47f20439f847d7bb70052
ljolia <elena.babko@gmail.com>
2023-09-14 10:25:06 +0300

    low fat and recyclable (with SQL)

--- 30DaysOfPandas/Data Filtering/RecyclableLowFatProducts.py
+++ 30DaysOfPandas/Data Filtering/RecyclableLowFatProducts.py
@@ -13,3 +13,10 @@ import pandas as pd
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     find_products_df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
     return find_products_df[['product_id']]
+
+
+# Write your MySQL query statement below
+
+SELECT product_id
+FROM products
+WHERE (low_fats = 'Y') AND (recyclable = 'Y');
