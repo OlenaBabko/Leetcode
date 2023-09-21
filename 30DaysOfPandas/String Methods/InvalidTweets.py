d323fa6b3912eba0fc2534371f4a423db3540bf7
Olena Babko <elena.babko@gmail.com>
2023-09-21 14:22:17 +0300

    find IDs of invalid tweets (with pandas)

---
+++ 30DaysOfPandas/String Methods/InvalidTweets.py
@@ -0,0 +1,17 @@
+# tweet_id is the primary key (column with unique values) for this table.
+# This table contains all the tweets in a social media app.
+# Write a solution to find the IDs of the invalid tweets. The tweet is
+# invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+# Return the result table in any order.
+
+
+# tweet is invalid if characters in  content > 15
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    invalid_tweets_df = tweets[tweets['content'].str.len('content') > 15]
+    invalid_tweets_id_df = invalid_tweets_df[['tweet_id']]
+    return invalid_tweets_id_df
+
+
