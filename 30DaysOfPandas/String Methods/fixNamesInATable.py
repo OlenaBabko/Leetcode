ebc3b022cf716af90eeade00071c52a48bbe2106
Olena Babko <elena.babko@gmail.com>
2023-09-25 15:44:09 +0300

    fix 1st to uppercase (with pandas)

---
+++ 30DaysOfPandas/String Methods/fixNamesInATable.py
@@ -0,0 +1,14 @@
+# user_id is the primary key (column with unique values) for this table.
+# This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+# Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+# Return the result table ordered by user_id.
+
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str.capitalize()
+    fix_names_sorted_df = users[['user_id','name']].sort_values('user_id')
+    return fix_names_sorted_df
+
+
+
