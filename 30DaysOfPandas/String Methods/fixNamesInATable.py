827908779195fbf0fffc04cfddf2c622db826bc2
Olena Babko <elena.babko@gmail.com>
2023-09-26 13:11:15 +0300

    fix 1st to uppercase (with SQL)

--- 30DaysOfPandas/String Methods/fixNamesInATable.py
+++ 30DaysOfPandas/String Methods/fixNamesInATable.py
@@ -12,3 +12,9 @@ def fix_names(users: pd.DataFrame) -> pd.DataFrame:
 
 
 
+# Write your MySQL query statement below
+
+SELECT user_id, CONCAT(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,length(name)))) AS name
+FROM Users
+ORDER BY user_id ASC;
+
