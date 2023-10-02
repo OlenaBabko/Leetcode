bbf70eff357d05f981ffa77444e37838dc66a637
Olena Babko <elena.babko@gmail.com>
2023-10-02 13:49:36 +0300

    find patient with type I diabet (with SQL)

--- 30DaysOfPandas/String Methods/PatientsWithACondition.py
+++ 30DaysOfPandas/String Methods/PatientsWithACondition.py
@@ -20,3 +20,15 @@ def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
 # The \b in the pattern is a word boundary assertion that ensures 'DIAB1' is a separate word and not part of another word
 
 
+
+
+
+# Write your MySQL query statement below
+SELECT *
+FROM Patients
+WHERE conditions REGEXP '\\bDIAB1';
+
+
+# \b matches either a non-word character (in our case, a space) or the position before the first character in the string.
+# Also, you need to escape a backslash with another backslash, like so: \\b. Otherwise, the regular expression won't evaluate.
+# \b also matches the position after the last character.
