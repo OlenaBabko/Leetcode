85f821ddea48466a63c30b64715ce3e333134e4d
Olena Babko <elena.babko@gmail.com>
2023-10-11 09:58:34 +0300

    if product is available in stores (with SQL)

--- 30DaysOfPandas/Data Manipulation/RearrangeProductsTable.py
+++ 30DaysOfPandas/Data Manipulation/RearrangeProductsTable.py
@@ -17,3 +17,19 @@ def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
     return drop_df
 
 
+
+
+# Write your MySQL query statement below
+
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
+ORDER BY product_id, store;
