4698cd9445df393a3b1c71e037233c05ea87ddfb
Olena Babko <elena.babko@gmail.com>
2023-10-11 09:57:45 +0300

    if product is available in some stores (with pandas)

---
+++ 30DaysOfPandas/Data Manipulation/RearrangeProductsTable.py
@@ -0,0 +1,19 @@
+# product_id is the primary key (column with unique values) for this table.
+# Each row in this table indicates the product's price in 3 different stores: store1, store2, and store3.
+# If the product is not available in a store, the price will be null in that store's column.
+# Write a solution to rearrange the Products table so that each row has (product_id, store, price). If a product 
+# is not available in a store, do not include a row with that product_id and store combination in the result table.
+# Return the result table in any order.
+
+
+# each row has (product_id, store, price), not available = do not include
+# pandas.melt(frame, id_vars=None, value_vars=None, var_name=None, value_name='value', col_level=None)
+
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    rearrange_df = pd.melt(products, id_vars='product_id', value_vars= ['store1', 'store2', 'store3'], var_name='store', value_name='price')
+    drop_df = rearrange_df.dropna()     # get rid of the null values
+    return drop_df
+
+
