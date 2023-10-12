7d2f2af028df2e2431823243c5d8a4d75fdb447e
Olena Babko <elena.babko@gmail.com>
2023-10-12 16:37:08 +0300

    find highest salarys in departments (with pandas)

---
+++ 30DaysOfPandas/Data Manipulation/DepartmentHighestSalary_m.py
@@ -0,0 +1,23 @@
+# id is the primary key (column with unique values) for this table.
+# departmentId is a foreign key (reference columns) of the ID from the Department table.
+# Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+#  id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+# Each row of this table indicates the ID of a department and its name.
+# Write a solution to find employees who have the highest salary in each of the departments.
+
+# Return the result table in any order.
+
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    employee_renamed = employee.rename(columns={'name': 'Employee', 'salary': 'Salary'})
+    department_renamed = department.rename(columns={'name': 'Department'})
+    merged = employee_renamed.merge(department_renamed, left_on='departmentId', right_on='id')
+    max_salary = merged.groupby('Department')['Salary'].transform(max)
+    max_in_department = merged[merged['Salary'] == max_salary]
+    dep_h_sal_df = max_in_department[['Department', 'Employee', 'Salary']]
+    return dep_h_sal_df
+
+
+
+
