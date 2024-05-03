74c3bc770d5ea6127c97daf0ceb55875b07dbfa6
Olena Babko <elena.babko@gmail.com>
2024-05-03 22:37:47 +0200

    customer who placed largest # of orders ( SQL)

--- 30DaysOfPandas/Data Aggregation/CustomerPlacingTheLargestNumberOfOrders.py
+++ 30DaysOfPandas/Data Aggregation/CustomerPlacingTheLargestNumberOfOrders.py
@@ -21,3 +21,12 @@ def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
 
 
 
+# Write your MySQL query statement below
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1
+
+
