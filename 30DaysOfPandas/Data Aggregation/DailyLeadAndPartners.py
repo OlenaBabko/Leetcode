69480679f54987bf56bd4e340ef01eb66e62fd8e
Olena Babko <elena.babko@gmail.com>
2024-05-08 13:42:55 +0300

    find lead id's and partner id's (pandas)

---
+++ 30DaysOfPandas/Data Aggregation/DailyLeadAndPartners.py
@@ -0,0 +1,20 @@
+# There is no primary key (column with unique values) for this table. It may contain duplicates.
+# This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
+# The name consists of only lowercase English letters.
+# For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.
+
+# Return the result table in any order.
+
+
+#
+import pandas as pd
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    grouped = daily_sales.groupby(['date_id', 'make_name']).nunique().reset_index()
+    renamed = grouped.rename(columns={'lead_id': 'unique_leads', 'partner_id': 'unique_partners'})
+    return renamed
+ # grouped.columns = ['date_id', 'make_name', 'unique_leads', 'unique_partners']
+
+
+
+
+
