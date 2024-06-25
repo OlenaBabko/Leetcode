d6b2942ff5e316b4243375f6f95f06ef9b7d43b0
Olena Babko <elena.babko@gmail.com>
2024-06-25 13:00:20 +0300

    show the unique ID of each user (Pandas)

---
+++ 30DaysOfPandas/Data Integration/ReplaceEmployeeIDWithTheUniqueIdentifier.py
@@ -0,0 +1,21 @@
+# id is the primary key (column with unique values) for Employees table.
+# Each row of this table contains the id and the name of an employee in a company.
+#
+# (id, unique_id) is the primary key (combination of columns with unique values) for EmployeeUNI table.
+# Each row of this table contains the id and the corresponding unique id of an employee in the company.
+# Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+# Return the result table in any order.
+
+
+### id from Employees =  id from EmployeeUNI => unique_id
+
+
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    merged = pd.merge(employees, employee_uni, on= 'id', how='left')    ### = employees.merge(employee_uni, on='id', how='left')
+    replaced = merged[['unique_id', 'name']]
+    return replaced
+
+
